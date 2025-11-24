650f731cb92d7d28fb0bc20dc6783677dacc80ec
Pedro Lima <145206042+k4pedro@users.roreply.github.com>
2025-11-23 23:27:44 -0300

    feat: adiciona leitura de arquivo e exibição de conteúdo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
+
 const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
-console.log(caminhoArquivo);
+fs.readFile(link, 'utf-8', (erro, texto) => {
+    console.log(texto);
+})
